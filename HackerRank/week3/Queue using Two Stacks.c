3066fde138f4d907f00b4e45a589771ee2e6cc42
Shree Raksha B N <133978927+Raksha25gowda@users.noreply.github.com>
2025-08-26 15:12:38 +0530

    Update Queue using Two Stacks.c

---
+++ HackerRank/week3/Queue using Two Stacks.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+        int q;
+    scanf("%d", &q);
+
+    int stack1[100000], stack2[100000];
+    int top1 = -1, top2 = -1;
+
+    while (q--) {
+        int type;
+        scanf("%d", &type);
+
+        if (type == 1) {
+            int x;
+            scanf("%d", &x);
+            stack1[++top1] = x;
+        } 
+        else if (type == 2) {
+            if (top2 == -1) {
+                while (top1 != -1) {
+                    stack2[++top2] = stack1[top1--];
+                }
+            }
+            if (top2 != -1) top2--;
+        } 
+        else if (type == 3) {
+            if (top2 == -1) {
+                while (top1 != -1) {
+                    stack2[++top2] = stack1[top1--];
+                }
+            }
+            if (top2 != -1) printf("%d\n", stack2[top2]);
+        }
+    }
+   
+    return 0;
+}
